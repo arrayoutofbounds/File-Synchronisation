2089f6e6dccdf83a286da358c708052b1dfd4e4c
Anmol Desai <ades597@aucklanduni.ac.nz>
2015-09-21 22:33:48 +1200

    updating local .sync file done

--- sync.py
+++ sync.py
@@ -51,6 +51,7 @@ def check(directory_1,directory_2):
 
 def makeSync(path):
 	a = open(path,"w+")
+	a.write("{\n}")
 	a.close()
 
 
@@ -75,6 +76,49 @@ def checkSyncFile(directory_1,directory_2):
 	return True	
 
 
+def addToLocalSyncFile(hex_dig,last_modified_date,dictionary,f):
+	''' this function takes in the calculated hash value, last modification date, values in current .sycn file and the file name'''
+
+	# purpose of this function is to update the .sync file.
+
+	#print(dictionary)
+
+	# check if the file name is in the dictionary as a key
+	if f in dictionary:
+		# then key exists, so first check the digest to see if same. If they are same then check mod time. else add to dictionary
+
+		# get the digest from the dictionary
+		values = dictionary[f]
+		latestValues = values[0]
+		latestDigest = latestValues[1]
+		latestModifiedTime = latestValues[0]
+		
+		if(latestDigest == hex_dig):
+			# old value is same as new value. So look at modification time now.
+			if(latestModifiedTime == last_modified_date):
+				#if the two strings are equal then the time also must be the same
+				pass
+			else:
+				# then the times are different, then replace the latest time (which is the last_modified_time that was passed in)
+				# latest values are known
+				latestValues[0] = last_modified_date
+
+		else:
+			# old value is not the same as new value
+			# hence add new value to the list of values	
+			dictionary[f] = [[last_modified_date,hex_dig]] + dictionary[f]
+
+	else:
+		# key is not in the dictionary. So add key, value pair to the dictionary. The dictionary is empty hence you only need to add the new pair and not
+		# worry about any other key that exists.
+		dictionary[f] = [[last_modified_date,hex_dig]]
+		#print("this is being done each time a new key is added")
+
+
+	return dictionary
+
+
+
 def updateSync(directory):
 	''' go through the list of files in the directory, get their content and write to .sync file in json format'''
 
@@ -85,35 +129,65 @@ def updateSync(directory):
 	#fileNames = os.listdir(directory)
 
 	# step 1: loop though each file and then call the digest method. 
-	for root,dirs,files in os.walk('./' + directory,topdown=True):
+	for root,dirs,files in os.walk('.' + os.sep + directory,topdown=True):
+
+		# create the dictionary that you will hold the values from the json file and use it to put to the json sync file.
+		#dictionary = {}
+
+		# open the .sync file and 
+		with open(directory + os.sep + '.sync') as g:
+			#dictionary = json.load(g)
+			try: 
+				dictionary = json.load(g)
+			except ValueError: 
+				#print("error caught")
+				dictionary = {}
+    
+		
+		# testing to ensure that something is read
+		#print(dictionary)
+
+		# now that the dict holds the information from the .sync file.
+
+		#objectives:
+		# 1. check if the file key exists in the dictionary
+		# 2. If it does, then get the first value compare the calculated hash value with the first hashed value
+		# 3. if the values are the same then check the modification time. If the modification times are same do thing, else just change mod time
+		# 4. deal with delete
 
 		# this got the files in the directory we entered
 		for f in files:
 
 			# if the file is a .sync file then do get information from it. i.e skip it as it does not need to be read.
 			if(f == ".sync"):
-				continue
+				continue		
 
 			# open a file
-			openedFile = open(directory + "/" + f, 'r')
+			openedFile = open(directory + os.sep + f, 'r')
 			text = openedFile.read()
-
 
 			# step 2: Store the digest method in the .sync file in json format
 			hash_object = hashlib.sha256(text.encode())
 			hex_dig = hash_object.hexdigest()
 			#print(hex_dig)
-			last_modified_date = time.strftime("%Y-%m-%d %H:%M:%S %z", time.gmtime(os.path.getmtime(directory + "/" + f)))
+			last_modified_date = time.strftime("%Y-%m-%d %H:%M:%S %z", time.gmtime(os.path.getmtime(directory + os.sep + f)))
+
 
 			#print (utils.formatdate(os.path.getmtime(directory + "/" + f))) # can use this for getting time zone also
 
-			print(last_modified_date)
+			# for testing
+			#print(last_modified_date)
 
-			dictionary = {f: [last_modified_date,1]}
+			# now call the check function. params to pass in are : new hashed value, the dictionary, file name
+			# all params in python are passed as reference
+			dictionary = addToLocalSyncFile(hex_dig,last_modified_date,dictionary,f)
+
+
+			#dictionary[f] = [last_modified_date,hex_dig]  this is a useless line
 
 
 			# open the .sync file in the directory given and then write to it in json format
-			with open(directory + "/.sync", 'w') as outfile:
+			with open(directory +  os.sep + ".sync", 'w') as outfile:
 				json.dump(dictionary, outfile,indent = 4)
 
 
@@ -154,7 +228,7 @@ def main():
 	# and use SHA 256 to create a digest and APPEND to the .sync file in that dir
 
 	updateSync(directory_1)
-
+	updateSync(directory_2)
 
 
 
